8be50bb98e0a8822ac320e1b3139f7da7be662d5
Khush Chaurasia <121889921+Khush0031@users.noreply.github.com>
2024-02-13 13:26:22 +0530

    13-Feb-2024 Uploading My first portfolio-project

---
+++ index.js
@@ -0,0 +1,28 @@
+  
+const backToTopButton = document.querySelector(".backToTop");
+let isBackToTopRendered = false;
+
+let alterStyles = (isBackToTopRendered) => {
+  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+  backToTopButton.style.transform = isBackToTopRendered
+    ? "scale(1)"
+    : "scale(0)";
+};
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    isBackToTopRendered = true;
+    alterStyles(isBackToTopRendered);
+  } else {
+    isBackToTopRendered = false;
+    alterStyles(isBackToTopRendered);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const themeToggleBtn = document.getElementById('themeToggle');
+  themeToggleBtn.addEventListener('change', function() {
+      document.body.classList.toggle('darkTheme');
+  });
+});
